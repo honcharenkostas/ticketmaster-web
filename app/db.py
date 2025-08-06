9b71c0d1d24b0b2f6bc50cb303c31867db2f6ab8
stas <honcharenko.stas@gmail.com>
2025-08-06 17:17:33 +0100

    initical commit. db

---
+++ app/db.py
@@ -0,0 +1,36 @@
+
+import os
+from datetime import datetime as dt
+from dotenv import load_dotenv
+from sqlalchemy import create_engine, Column, Integer, String, DateTime
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+
+load_dotenv()
+DATABASE_URL = f"postgresql://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOST")}:{os.getenv("DB_PORT")}/{os.getenv("DB_NAME")}"
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+Base = declarative_base()
+
+
+# Models
+class Event(Base):
+    __tablename__ = "events"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    encsoft_url = Column(String)
+    cvv = Column(String)
+    created_at = Column(DateTime, default=dt.utcnow)
+    updated_at = Column(DateTime, default=dt.utcnow, onupdate=dt.utcnow)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+Base.metadata.create_all(bind=engine)
