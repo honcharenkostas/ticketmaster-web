8d45e75a852fb576ce4f16a4420b9b3c0b2cee24
stas <honcharenko.stas@gmail.com>
2025-08-11 13:35:53 +0100

    discord listener

--- app/discord_listener.py
+++ app/discord_listener.py
@@ -1,8 +1,9 @@
-import discord
+import requests
 import os
+import time
 import logging
 from datetime import datetime
-from db import Event, EventDetails, BotAccount, get_db
+from db import Event, EventDetails, BotAccount, SessionLocal
 from dotenv import load_dotenv
 
 
@@ -17,77 +18,99 @@ logging.basicConfig(
     ]
 )
 logger = logging.getLogger(__name__)
-db = get_db()
-intents = discord.Intents.default()
-intents.messages = True
-intents.message_content = True  # Required to read message text
-client = discord.Client(intents=intents)
-
-@client.event
-async def on_ready():
-    logger.info(f"Logged in as {client.user}")
-
-@client.event
-async def on_message(msg):
-    if msg.channel.id == os.getenv("DISCORD_CHANNEL_ID"):
-        logger.info(f"[{msg.author}] {msg.content}")
-
-        # prepare data
-        data = dict()
-        try:
-            for row in msg.content["embeds"][0]["fields"]:
-                name = row.get("name")
-                val = row.get("value")
-                if name and val:
-                    data[name] = val
-        except Exception as e:
-            logger.error(e)
-            return
-        
-        # validate
-        required = {"Event ID", "Account", "Section", "Row", "Price", "Full price", "Amount", "Expiration", "Full checkout"}
-        for k in required:
-            if not data.get(k):
-                logger.error(f"Field {k} is required")
-                return
-
-        # enrich
-        event_details = db.query(EventDetails).filter(EventDetails.event_id == data["Event ID"]).first()
-        event_name = event_details.event_name if event_details else None
-        bot = db.query(BotAccount).filter(BotAccount.email == data["Account"]).first()
-        cvv = bot.cvv if bot else None
-        full_price = round(float(data["Full price"]), 2)
-        amount = int(data["Amount"])
-        if not full_price or not amount:
-            logger.error("Invalid full price or amount")
-            return
-        
-        price_plus_fees = round(full_price / amount, 2)
-        expire_at = None
-        try:
-            expire_at = datetime.fromtimestamp(int(data["Expiration"].replace("<t:", "").replace(":R>", "")))
-        except Exception as e:
-            logger.error(e)
-            return
-
-        event = Event(
-            event_id=data["Event ID"],
-            event_name=event_name,
-            bot_email=data["Account"],
-            section=data["Section"],
-            row=data["Row"],
-            price=float(data["Price"]),
-            amount=int(data["Amount"]),
-            full_price=float(data["Full price"]),
-            price_plus_fees=price_plus_fees,
-            expire_at=expire_at,
-            encsoft_url=data["Full checkout"],
-            cvv=cvv,
-            status=Event.STATUS_NEW
-        )
-        db.add(event)
-        db.commit()
-        db.refresh(event)
+db = SessionLocal()
+ids = set()
 
 
-client.run(os.getenv("DISCORD_BOT_TOKEN"))
+def run():
+    try:
+        url = f"https://discord.com/api/v10/channels/{os.getenv('DISCORD_CHANNEL_ID')}/messages"
+        headers = {
+            "Authorization": f"Bot {os.getenv('DISCORD_BOT_TOKEN')}"
+        }
+        params = {
+            "limit": 50  # how many messages to fetch, max 100
+        }
+
+        response = requests.get(url, headers=headers, params=params)
+
+        if response.status_code == 200:
+            messages = response.json()
+            for msg in messages:
+                id = msg.get("id")
+                if not id:
+                    continue
+
+                if id in ids:
+                    continue
+
+                ids.add(id)
+
+                # prepare data
+                data = dict()
+                try:
+                    for row in msg["embeds"][0]["fields"]:
+                        name = row.get("name")
+                        val = row.get("value")
+                        if name and val:
+                            data[name] = val
+                except Exception as e:
+                    logger.error(e)
+                    return
+                
+                # validate
+                required = {"Event ID", "Account", "Section", "Row", "Price", "Full price", "Amount", "Expiration", "Full checkout"}
+                for k in required:
+                    if not data.get(k):
+                        logger.error(f"Field {k} is required")
+                        return
+
+                # enrich
+                event_details = db.query(EventDetails).filter(EventDetails.event_id == data["Event ID"]).first()
+                event_name = event_details.event_name if event_details else None
+                bot = db.query(BotAccount).filter(BotAccount.email == data["Account"]).first()
+                cvv = bot.cvv if bot else None
+                full_price = round(float(data["Full price"]), 2)
+                amount = int(data["Amount"])
+                if not full_price or not amount:
+                    logger.error("Invalid full price or amount")
+                    return
+                
+                price_plus_fees = round(full_price / amount, 2)
+                expire_at = None
+                try:
+                    expire_at = datetime.fromtimestamp(int(data["Expiration"].replace("<t:", "").replace(":R>", "")))
+                except Exception as e:
+                    logger.error(e)
+                    return
+
+                event = Event(
+                    event_id=data["Event ID"],
+                    event_name=event_name,
+                    bot_email=data["Account"],
+                    section=data["Section"],
+                    row=data["Row"],
+                    price=float(data["Price"]),
+                    amount=int(data["Amount"]),
+                    full_price=float(data["Full price"]),
+                    price_plus_fees=price_plus_fees,
+                    expire_at=expire_at,
+                    encsoft_url=data["Full checkout"],
+                    cvv=cvv,
+                    status=Event.STATUS_NEW
+                )
+                db.add(event)
+                db.commit()
+                db.refresh(event)
+        else:
+            logger.error("Error:", response.status_code, response.text)
+    except Exception as e:
+        logger.error(e)
+
+try:
+    while True:
+        logger.info("Get messages...")
+        run()
+        time.sleep(1)
+except KeyboardInterrupt:
+    logger.info("Shutting down ...")
